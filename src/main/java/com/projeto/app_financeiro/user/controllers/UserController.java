f511346adeff6c48508deac91f0f2330c60cf8fa
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-06-07 18:49:07 -0300

    arrumando role registro usuário

--- src/main/java/com/projeto/app_financeiro/user/controllers/UserController.java
+++ src/main/java/com/projeto/app_financeiro/user/controllers/UserController.java
@@ -1,6 +1,10 @@
 package com.projeto.app_financeiro.user.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,25 +12,35 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.projeto.app_financeiro.user.dto.UserDTO;
 import com.projeto.app_financeiro.user.entities.UserEntity;
-import com.projeto.app_financeiro.user.services.UserService;
+import com.projeto.app_financeiro.user.enums.UserRoleEnum;
+import com.projeto.app_financeiro.user.repositories.UserRepository;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
     @Autowired
-    private UserService userService;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping("/register")
-    public UserDTO registerUser(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<?> registerUser(@RequestBody @Valid UserDTO userDTO) {
+
+        if (this.userRepository.findByUsername(userDTO.getUsername()).isPresent()) {
+            return ResponseEntity.badRequest().body("Username already exists");
+        }
+        String encryptedPassword = passwordEncoder.encode(userDTO.getPassword());
+        
         UserEntity user = new UserEntity();
         user.setUsername(userDTO.getUsername());
         user.setEmail(userDTO.getEmail());
-        user.setPassword(userDTO.getPassword());
+        user.setPassword(encryptedPassword);
+        user.setRole(UserRoleEnum.USER);
 
-        UserEntity registredUser = userService.registerUser(userDTO);
-        userDTO.setId(registredUser.getId());
-        return userDTO;
+        this.userRepository.save(user);
+        return ResponseEntity.ok("User registered successfully");
     }
-
     
 }
